e3b8cfd64f025e8e8151c059828fe67026cc0366
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2016-11-10 12:40:01 +0100

    Speed bump in fps

--- renderers/cudaRaycaster/cuda/Renderer.cu
+++ renderers/cudaRaycaster/cuda/Renderer.cu
@@ -53,12 +53,12 @@ inline __device__ float4 calcPositionInEyeSpaceFromWindowSpace( const float2& wi
 // intersect ray with a box
 // http://www.siggraph.org/education/materials/HyperGraph/raytrace/rtinter3.htm
 // implementation from nvidia sample
-__device__ bool intersectBox( const float3& origin,
-                              const float3& dir,
-                              const float3& boxMin,
-                              const float3& boxMax,
-                              float *tnear,
-                              float *tfar )
+inline __device__ bool intersectBox( const float3& origin,
+                                     const float3& dir,
+                                     const float3& boxMin,
+                                     const float3& boxMax,
+                                     float *tnear,
+                                     float *tfar )
 {
     // compute intersection of ray with all six bbox planes
     const float3& invR = recp( dir );
@@ -85,7 +85,8 @@ inline __device__ float4 composite( const float4& src,
                                     const float alphaCorrection )
 {
     // The alpha correction function behaves badly around maximum alpha
-    const float alpha = 1.0f - pow( 1.0f - min( src.w, 1.0f - 1.0f / 256.0 ), alphaCorrection );
+    const float corr = 1.0f - fminf( src.w, 1.0f - 1.0f / 256.0 );
+    const float alpha = 1.0f - pow( corr , alphaCorrection );
     const float3& xyz = make_float3( dst ) + make_float3( src ) * alpha * ( 1.0 - dst.w );
     const float dstw = dst.w + alpha * ( 1.0f - dst.w );
     return {  xyz.x, xyz.y, xyz.z, dstw };
@@ -273,11 +274,11 @@ struct Renderer::Impl
                  const cuda::TexturePool& texturePool )
     {
 
+
         const unsigned int width = _cudaRenderPBO.getWidth();
         const unsigned int height = _cudaRenderPBO.getHeight();
 
         copyNodeDatasToCudaMemory( nodeDatas );
-
         const dim3 blockDim( 16, 16 );
         const dim3 gridDim( iDivUp( width, blockDim.x ), iDivUp( height, blockDim.y ));
         rayCast<<< gridDim, blockDim >>>( texturePool.getTexture(),
